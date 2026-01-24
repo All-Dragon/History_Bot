15d2bb6b26dc366fffebd360aafa840d82b6414e
All-Dragon <luch2006al@gmail.com>
2026-01-24 23:24:09 +0500

    Вывод фото к рандомным вопросам

--- Bot/handlers/get_questions_handler.py
+++ Bot/handlers/get_questions_handler.py
@@ -25,18 +25,27 @@ config: Config = load_config()
 
 get_question_router = Router()
 
+async def delete_user_message(message: Message):
+    try:
+        await message.delete()
+    except Exception:
+        pass
 
-def get_markup_options(options: list[Any]):
+
+
+def get_markup_options(options: list[Any] = []):
     kb_builder = InlineKeyboardBuilder()
 
 
-    for i, text in enumerate(options):
-        kb_builder.button(
-            text=text,
-            callback_data=AnswerCD(chosen=i).pack()
-        )
 
-    kb_builder.adjust(4)
+    if len(options) > 1:
+        for i, text in enumerate(options):
+            kb_builder.button(
+                text=text,
+                callback_data=AnswerCD(chosen=i).pack()
+            )
+
+        kb_builder.adjust(2)
 
     kb_builder.row(
         InlineKeyboardButton(
@@ -103,25 +112,31 @@ async def get_random_question(message: Message, state: FSMContext):
                     keyboard = get_markup_options(options)
 
                     if data['image_url']:
+                        await state.set_state(AnswerState.answer_waiting)
                         await message.answer_photo(
                             photo = data['image_url'],
                             caption= text,
                             reply_markup= keyboard
                         )
                     else:
+                        await state.set_state(AnswerState.answer_waiting)
                         await message.answer(text = text, reply_markup=keyboard)
 
                 if q_type == 'free_text':
 
                     if data['image_url']:
-                        await message.answer_photo(
+                        sent_msg = await message.answer_photo(
                             photo = data['image_url'],
-                            caption= text
+                            caption= text,
+                            reply_markup= get_markup_options()
                         )
+                        await state.update_data(question_msg_id = sent_msg.message_id)
                         await state.set_state(AnswerState.answer_waiting)
 
-                    await message.answer(text + "\n\nВаш ответ (введите текст):")
-                    await state.set_state(AnswerState.answer_waiting)
+                    else:
+                        sent_msg = await message.answer(text + "\n\nВаш ответ (введите текст):", reply_markup= get_markup_options())
+                        await state.update_data(question_msg_id = sent_msg.message_id)
+                        await state.set_state(AnswerState.answer_waiting)
     except Exception as e:
         await message.answer(str(e))
 
@@ -145,20 +160,31 @@ async def process_multiple_answer(callback: CallbackQuery, state: FSMContext, ca
     user_answer = options[chosen_index]
     correct = questions['correct_answer']
 
-    if user_answer == correct:
-        await callback.message.answer("Правильно! 🎉")
-    else:
-        await callback.message.answer(f"Неправильно 😔 Правильный ответ: {correct}")
+    q_type = data.get('question_type')
+    type_text = "С вариантами ответов" if q_type == 'multiple_choice' else 'Свободный ответ'
 
+    result_text = "Правильно! 🎉" if user_answer == correct else "Неправильно 😔"
+    correct_text = f'Правильный ответ: {correct}'
+
+    text = (f'{result_text}\n'
+            f'Ваш ответ: {user_answer}\n'
+            f'{correct_text}\n\n'
+            f'Тип вопроса: {type_text}\n'
+            f'Оригинальный вопрос:\n\n'
+            f'{questions['text']}')
+
+    if questions['image_url']:
+        await callback.message.edit_caption(
+            caption= text
+        )
+
+    else:
+        await callback.message.edit_text(
+            text = text,
+            reply_markup= None
+        )
     await state.clear()
     await callback.message.answer("Следующий вопрос? /random")
-
-@get_question_router.callback_query(F.data == 'cancel_question')
-async def cancel_questions(callback: CallbackQuery, state: FSMContext):
-    await callback.answer()
-    await state.clear()
-    await callback.message.edit_text("Вопрос отменён.\nНовый вопрос: /random")
-
 
 @get_question_router.message(StateFilter(AnswerState.answer_waiting))
 async def process_free_answer(message: Message, state: FSMContext):
@@ -172,11 +198,55 @@ async def process_free_answer(message: Message, state: FSMContext):
     user_answer = message.text.strip()
     correct = questions['correct_answer']
 
-    if user_answer.lower() == correct.lower():
-        await message.answer("Правильно! 🎉")
-    else:
-        await message.answer(f"Неправильно 😔 Правильный ответ: {correct}")
+    q_type = data.get('question_type')
+    type_text = "С вариантами ответов" if q_type == 'multiple_choice' else 'Свободный ответ'
+
+    result_text = "Правильно! 🎉" if user_answer.lower() == correct.lower() else "Неправильно 😔"
+    correct_text = f'Правильный ответ: {correct}'
+    question_msg_id = data.get('question_msg_id')
+
+    text = (
+        f'{result_text}\n'
+        f'Ваш ответ: {user_answer}\n'
+        f'{correct_text}\n\n'
+        f'Тип вопроса: {type_text}\n'
+        f'Оригинальный вопрос:\n\n'
+        f'{questions['text']}'
+    )
+
+    if question_msg_id:
+        if questions['image_url']:
+            await message.bot.edit_message_caption(
+                chat_id = message.chat.id,
+                message_id= question_msg_id,
+                caption = text,
+                reply_markup= None
+            )
+            await delete_user_message(message)
+
+        else:
+            await message.bot.edit_message_text(
+                chat_id= message.chat.id,
+                message_id= question_msg_id,
+                text = text,
+                reply_markup= None
+            )
+            await delete_user_message(message)
+        await message.answer("Следующий вопрос? /random")
 
     await state.clear()
-    await message.answer("Следующий вопрос? /random")
+
+@get_question_router.callback_query(StateFilter(AnswerState.answer_waiting) ,F.data == 'cancel_question')
+async def cancel_questions(callback: CallbackQuery, state: FSMContext):
+    await callback.answer()
+    await state.clear()
+
+    try:
+        await callback.message.delete()
+    except Exception:
+        pass
+
+    await callback.message.answer(
+        "Вопрос отменён.\nНовый вопрос: /random"
+    )
 
