7300812554e82b8a5164dfe9cdda686173120902
All-Dragon <luch2006al@gmail.com>
2026-03-29 01:53:26 +0500

    refactor(users): вынес логику в service и repository слой

--- API/routers/users_router/users_router.py
+++ API/routers/users_router/users_router.py
@@ -4,6 +4,7 @@ from Database.models import *
 from fastapi import Depends, status, HTTPException, APIRouter
 from JWT.auth import require_role, get_current_user
 import logging
+from services.user_service import UserService
 
 logger = logging.getLogger(__name__)
 
@@ -16,15 +17,7 @@ users_router = APIRouter(
 async def get_all_users(session: AsyncSession = Depends(get_async_session),
                         current_user: Users = Depends(require_role('Админ'))):
     logger.info('Получение информации о всех пользователях')
-    result = await session.scalars(select(Users))
-    result = result.all()
-
-    if not result:
-        logger.error('В БД нет пользователей')
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='В базе ещё нет пользователей')
-
-    logger.info(f'Данные о пользователях успешно получены: {len(result)} записей')
-    return result
+    return await UserService.get_all(session = session)
 
 @users_router.get('/me', response_model= User_Out)
 async def get_users_info(current_user: Users = Depends(get_current_user)):
@@ -34,93 +27,38 @@ async def get_users_info(current_user: Users = Depends(get_current_user)):
 
 @users_router.post('/create', response_model= ReadUser, status_code= status.HTTP_201_CREATED)
 async def create_new_user(data: CreateUser, session: AsyncSession = Depends(get_async_session)):
-    logger.info('Проверка на существование пользователя: %s', data.telegram_id)
-    double = await session.scalar(select(Users).where(Users.telegram_id == data.telegram_id))
-    if double:
-        logger.error('Пользователь с таким telegram_id: %s уже существует!', data.telegram_id)
-        raise HTTPException(status_code= status.HTTP_400_BAD_REQUEST, detail= 'Пользователь уже существует!')
-
-    logger.info('Создание нового пользователя')
-    new_user = Users(
-        telegram_id = data.telegram_id,
-        username = data.username,
-        role = data.role,
-        is_banned = data.is_banned
-    )
-
-    try:
-        session.add(new_user)
-        await session.commit()
-        await session.refresh(new_user)
-        logger.info('Пользователь %s успешно создан!', data.telegram_id)
-    except Exception as e:
-        logger.exception('Ошибка создания пользователя %s', data.telegram_id)
-        raise HTTPException(status_code=500, detail='Ошибка сервера')
-
+    new_user = await UserService.create(data = data, session = session)
     return ReadUser.model_validate(new_user)
-
 
 @users_router.put('/me/name', response_model= User_Out)
 async def change_name(data: ChangeName,
                       session: AsyncSession = Depends(get_async_session),
                       current_user: Users = Depends(get_current_user)):
-    logger.info('Смена имени пользователя %s', current_user.telegram_id)
-    try:
-        current_user.username = data.name
-        await session.commit()
-        await session.refresh(current_user)
-        logger.info('Успешная смена имени пользователя %s', current_user.telegram_id)
-    except Exception as e:
-        logger.exception('Ошибка изменения имени пользователя %s', current_user.telegram_id)
-        raise HTTPException(status_code= status.HTTP_500_INTERNAL_SERVER_ERROR, detail= 'Ошибки сервера')
-    return User_Out.model_validate(current_user)
+    updated_user = await UserService.change_name(
+        session = session,
+        current_user = current_user,
+        new_name = data.name
+    )
+    return User_Out.model_validate(updated_user)
 
 @users_router.put('/change/{telegram_id}', response_model= ReadUser)
 async def change(telegram_id: int,
                  data: Change_User,
                  session: AsyncSession = Depends(get_async_session),
                  current_user = Depends(require_role('Админ'))):
-    logger.info('Изменение данных пользователя %s', telegram_id)
-    user = await session.scalar(select(Users).where(Users.telegram_id == telegram_id))
-    if user is None:
-        logger.exception('Пользователя с таким id: %s, не существует!', telegram_id)
-        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail= 'Пользователь с таким telegram_id не найден')
-
-    updated_data = data.model_dump(exclude_unset=True, exclude_none= True)
-
-    for field, value in updated_data.items():
-        setattr(user, field, value)
-
-    try:
-        await session.commit()
-        await session.refresh(user)
-        logger.info('Успешное изменение данных о пользователе %s', telegram_id)
-    except Exception as e:
-        logger.exception('Ошибка изменения данных пользователя %s', telegram_id)
-
-    return ReadUser.model_validate(user)
+    updated_user = await UserService.change(
+        telegram_id = telegram_id,
+        session = session,
+        data = data
+    )
+    return ReadUser.model_validate(updated_user)
 
 @users_router.delete('/hard_del/{telegram_id}', status_code= status.HTTP_204_NO_CONTENT)
 async def hard_delete_user(
         telegram_id: int,
         session: AsyncSession = Depends(get_async_session),
         current_user: Users = Depends(require_role('Админ'))):
-    logger.info('Удаление пользователя %s', telegram_id)
-    item_to_del = await session.scalar(select(Users).where(Users.telegram_id == id))
-    if item_to_del is None:
-        logger.error('Такого пользователя не существует %s', telegram_id)
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= 'Такого пользователя не существует')
-
-    await session.delete(item_to_del)
-    try:
-        await session.commit()
-        logger.info('Успешное удаление пользователя: %s', telegram_id)
-    except Exception as e:
-        await session.rollback()
-        logger.exception('Ошибка при удалении пользователя %s', telegram_id)
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Database error during commit")
-
-    return
+    return await UserService.hard_delete_user(session = session, current_user = current_user)
 
 @users_router.delete('/soft_del',
                      status_code = status.HTTP_204_NO_CONTENT,
@@ -128,65 +66,16 @@ async def hard_delete_user(
 async def soft_delete_user(
         current_user: Users = Depends(get_current_user),
         session: AsyncSession = Depends(get_async_session)):
-    logger.info('Soft delete пользователя %s', current_user.telegram_id)
-    telegram_id = current_user.telegram_id
-    result = await session.execute(
-        update(Users)
-        .where(
-            Users.by_telegram_id(telegram_id),
-               Users.active()
-           ).values(deleted_at = datetime.now(timezone.utc))
-        .returning(Users.telegram_id)
-    )
+    return await UserService.soft_delete_user(session = session, current_user = current_user)
 
-    if not result:
-        logger.error('Пользователь %s не найден', current_user.telegram_id)
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="User not found or already deleted"
-        )
-    await session.commit()
-    logger.info('Успешный Soft delete пользователя %s', current_user.telegram_id)
-    return
 @users_router.put('/restore_user', response_model= ReadUser)
 async def restore_user(
         session: AsyncSession = Depends(get_async_session),
         current_user: Users = Depends(get_current_user)):
-    logger.info('Восстановление пользователя %s', current_user.telegram_id)
-    telegram_id = current_user.telegram_id
-
-    user = await session.scalar(
-        select(Users).where(
-            Users.by_telegram_id(telegram_id),
-            Users.non_active()
-        )
-    )
-    
-    if user is None:
-        logger.warning('Пользователь %s не найден', current_user.telegram_id)
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="User not found or not deleted"
-        )
-
-    user.deleted_at = None
-    try:
-        await session.commit()
-        await session.refresh(user)
-        logger.info('Успешное восстановление пользователя %s', current_user.telegram_id)
-    except Exception as e:
-        await session.rollback()
-        logger.exception('Ошибка при восстановлении пользователя %s', telegram_id)
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Ошибка сервера")
+    user = await UserService.restore_user(session = session, current_user = current_user)
     return ReadUser.model_validate(user)
 
 @users_router.get('/{telegram_id}')
 async def get_user(telegram_id: int,
                    session: AsyncSession = Depends(get_async_session)):
-    logger.info('Получение данных о пользователе %s', telegram_id)
-    result = await session.scalar(select(Users).where(Users.by_telegram_id(telegram_id)))
-    if result is None:
-        logger.error('Пользователь %s не найден', telegram_id)
-        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail= f'Пользователь с таким telegram_id не найден: {telegram_id}')
-    logger.info('Данные о пользователе %s успешно получены', telegram_id)
-    return result
+    return await UserService.get_by_telegram_id(session = session, telegram_id = telegram_id)
---
+++ repositories/users_router.py
@@ -0,0 +1,108 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from Database.models import Users
+from datetime import datetime, timezone
+
+class UserRepository:
+    @staticmethod
+    async def get_all_user(
+            session: AsyncSession
+    ) -> list[Users] | None:
+        return await session.scalars(select(Users))
+
+    @staticmethod
+    async def get_by_telegram_id(session: AsyncSession,
+                                 telegram_id: int):
+        return await session.scalar(select(Users).where(Users.telegram_id == telegram_id))
+
+    @staticmethod
+    async def change_name(
+            session: AsyncSession,
+            user: Users,
+            new_name: str
+    ):
+        user.username = new_name
+        await session.commit()
+        await session.refresh(user)
+        return user
+
+    @staticmethod
+    async def change(
+            session: AsyncSession,
+            user: Users,
+            updated_data: dict
+    ):
+        for field, value in updated_data.items():
+            setattr(user, field, value)
+
+        await session.commit()
+        await session.refresh(user)
+        return user
+
+
+    @staticmethod
+    async def get_active_user(
+            session: AsyncSession,
+            telegram_id: int
+    ):
+        user = await session.scalar(
+            select(Users).where(Users.telegram_id == telegram_id, Users.active())
+        )
+        return user
+
+    @staticmethod
+    async def get_deleted_user(
+            session: AsyncSession,
+            telegram_id: int
+    ):
+        user = await session.scalar(
+            select(Users).where(Users.telegram_id == telegram_id, Users.non_active())
+        )
+        return user
+
+    @staticmethod
+    async def restore_user(
+            session: AsyncSession,
+            user: Users
+    ):
+        user.deleted_at = None
+        try:
+            await session.commit()
+            await session.refresh(user)
+        except Exception as e:
+            await session.rollback()
+        return user
+
+
+    @staticmethod
+    async def soft_delete_user(
+            session: AsyncSession,
+            user: Users
+    ):
+        user.deleted_at = datetime.now(timezone.utc)
+        await session.commit()
+        await session.refresh(user)
+        return user
+
+    @staticmethod
+    async def hard_delete_user(
+            session: AsyncSession,
+            user: Users
+    ):
+        await session.delete(user)
+        try:
+            await session.commit()
+        except Exception as e:
+            await session.rollback()
+        return
+
+
+
+'''result = await session.execute(
+        update(Users)
+        .where(
+            Users.by_telegram_id(telegram_id),
+               Users.active()
+           ).values(deleted_at = datetime.now(timezone.utc))
+        .returning(Users.telegram_id)
+    )'''
